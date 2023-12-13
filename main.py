fe8c2062ad78805c16b9cc02994e896d7b6baf44
spencer0124 <spencer0124@naver.com>
2023-12-13 15:58:41 +0900

    test

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+import httpx
 
 app = FastAPI()
 a = 3
+URL = "http://ws.bus.go.kr/api/rest/buspos/getBusPosByRouteSt?serviceKey=ORCkFmKr8bIoQOxjPIhZsu4xEumjEQFC9cFW%2Br6C026Yk2LMhxAsuEb%2BYVShmoMzD8HHW257I92FA8slrJUQMg%3D%3D&busRouteId=100900004&startOrd=1&endOrd=19&resultType=json"
+
+@app.get("/bus/bus1")
+def request():
+    response = httpx.get(URL)
+    return {'response': response.json()}
+
 
 @app.get("/home/{name}")
 def read_name(name:str):
